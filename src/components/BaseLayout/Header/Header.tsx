a9b5fbf372daf477b7d3ed90cf8081f0ec5fbaf7
Jun Le <tayjunle@gmail.com>
2022-03-24 15:29:20 +0800

    add simple logout and fix notifications

--- src/components/BaseLayout/Header/Header.tsx
+++ src/components/BaseLayout/Header/Header.tsx
@@ -1,14 +1,33 @@
 import * as React from 'react';
-import { Avatar, Layout } from 'antd';
+import { LogoutOutlined } from '@ant-design/icons';
+import { Avatar, Dropdown, Layout, Menu } from 'antd';
 import { useStores } from '../../../stores/StoreProvider';
 import styles from '../BaseLayout.module.css';
 import { HeaderTabs } from './HeaderTabs';
 import { observer } from 'mobx-react';
+import { useRouter } from 'next/router';
 
 type HeaderProps = {};
 
-const Header: React.FC<HeaderProps> = ({}) => {
+const Header: React.FC<HeaderProps> = ({ }) => {
     const { appStore } = useStores();
+
+    const router = useRouter();
+
+    const onLogout = () => {
+        router.push('/login');
+        appStore.setCurrentUser({ name: '' });
+        appStore.setIsAuthenticated('');
+        sessionStorage.removeItem('authenticated');
+        sessionStorage.removeItem('user');
+    }
+
+    const menu =
+        <Menu>
+            <Menu.Item onClick={onLogout} icon={<LogoutOutlined />}>
+                Logout
+            </Menu.Item>
+        </Menu>
 
     return (
         <Layout.Header>
@@ -20,20 +39,22 @@ const Header: React.FC<HeaderProps> = ({}) => {
                 </div>
                 <HeaderTabs />
                 <div className={styles.profileContainer}>
-                    <Avatar
-                        alt="Profile Image"
-                        style={{
-                            backgroundColor: 'white',
-                            color: '#193D61',
-                            fontSize: '14px',
-                            cursor: 'pointer',
-                        }}
-                        shape="circle"
-                        size={24}
-                    >
-                        {appStore.currentUser?.name?.charAt(0).toUpperCase() ||
-                            'U'}
-                    </Avatar>
+                    <Dropdown overlay={menu} placement="bottomRight">
+                        <Avatar
+                            alt="Profile Image"
+                            style={{
+                                backgroundColor: 'white',
+                                color: '#193D61',
+                                fontSize: '14px',
+                                cursor: 'pointer',
+                            }}
+                            shape="circle"
+                            size={24}
+                        >
+                            {appStore.currentUser?.name?.charAt(0).toUpperCase() ||
+                                'U'}
+                        </Avatar>
+                    </Dropdown>
                 </div>
             </div>
         </Layout.Header>
